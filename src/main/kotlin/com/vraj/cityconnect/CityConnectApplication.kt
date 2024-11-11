d01c815beb83cf04a313ed6839f51f11345d50be
Vraj Shah <shahvraj2016@gmail.com>
2024-11-11 15:18:02 -0400

    Initial commit

---
+++ src/main/kotlin/com/vraj/cityconnect/CityConnectApplication.kt
@@ -0,0 +1,11 @@
+package com.vraj.cityconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CityConnectApplication
+
+fun main(args: Array<String>) {
+    runApplication<CityConnectApplication>(*args)
+}
